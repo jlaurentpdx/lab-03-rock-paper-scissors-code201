5e2ca16ff221fd6887205ecff03f9233288a13e8
Jordan Laurent <jlaurent.pdx@gmail.com>
2021-10-06 17:45:22 -0700

    added JS DOM elements

---
+++ app.js
@@ -0,0 +1,7 @@
+const playButton = document.getElementById('play');
+const result = document.getElementById('result');
+const wins = document.getElementById('wins');
+const losses = document.getElementById('losses');
+const draws = document.getElementById('draws');
+const error = document.getElementById('error');
+
