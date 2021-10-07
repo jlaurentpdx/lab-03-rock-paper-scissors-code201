117674314f4b7e3abcf5f63324076469f061c094
Jordan Laurent <jlaurent.pdx@gmail.com>
2021-10-07 01:09:47 -0700

    added and implemented CSS animations

--- app.js
+++ app.js
@@ -6,6 +6,9 @@ const winCount = document.getElementById('win-count');
 const lossCount = document.getElementById('loss-count');
 const tieCount = document.getElementById('tie-count');
 const error = document.getElementById('error');
+const bulbImg = document.getElementById('bulb-img');
+const charImg = document.getElementById('char-img');
+const squiImg = document.getElementById('squi-img');
 
 let wins = 0;
 let losses = 0;
@@ -20,26 +23,35 @@ playButton.addEventListener('click', () => {
     const userChoice = chosen.value;
     const cpuChoice = getRandomChoice();
 
+    const applyAnimation = (winner) => {
+        bulbImg.classList.remove('win');
+        charImg.classList.remove('win');
+        squiImg.classList.remove('win');
+
+        if (winner === 'grass') {
+            bulbImg.classList.add('win');} 
+        else if (winner === 'fire') {
+            charImg.classList.add('win');}
+        else if (winner === 'water') {
+            squiImg.classList.add('win');}
+        }
+
     if (doesUserWin(userChoice, cpuChoice) === 'tie'){
         ties++; tieCount.textContent = ties; 
         result.textContent = 'You tied!'
+        result.style.color = 'lightgray';
+        applyAnimation(null)
     } else if (doesUserWin(userChoice, cpuChoice) === 'win') {
         wins++; winCount.textContent = wins; 
         result.textContent = 'You won!';
-    } else if (doesUserWin(userChoice, cpuChoice) === 'lose') {
-        losses++; lossCount.textContent = losses; 
-        result.textContent = 'You lost!'
+        result.style.color = 'green';
+        applyAnimation(userChoice);
     } else {
-        console.log('weird bug');
+        losses++; lossCount.textContent = losses; 
+        result.textContent = 'You lost!';
+        result.style.color = 'red';
+        applyAnimation(cpuChoice);
     }
-
-    // function applyFilter(cpuChoice) {
-    //     cpuChoice.classList.remove('player-lost', 'player-win');
-    //     cpuChoice.classList.remove('player-lost');
-    //     cpuChoice.classList.add('player-win');
-    //     cpuChoice.classList.remove('player-win');
-    //     cpuChoice.classList.add('player-lost');
-    // }
 })
 
 
