21f23d60f613d262e1e47ccbba7e945a345d48db
Jordan Laurent <jlaurent.pdx@gmail.com>
2021-10-06 19:21:47 -0700

    added successful TDD testing for win/lose/tie scenarios

--- app.js
+++ app.js
@@ -1,7 +1,16 @@
+import { doesUserWin, getRandomChoice } from "./utilities.js";
+
 const playButton = document.getElementById('play');
 const result = document.getElementById('result');
-const wins = document.getElementById('wins');
-const losses = document.getElementById('losses');
-const draws = document.getElementById('draws');
+const winCount = document.getElementById('win-count');
+const lossCount = document.getElementById('loss-count');
+const tieCount = document.getElementById('tie-count');
 const error = document.getElementById('error');
 
+let wins = 0;
+let losses = 0;
+let ties = 0;
+
+playButton.addEventListener('click', () => {
+    
+})
--- test/example.test.js
+++ test/example.test.js
@@ -1,18 +1,64 @@
 // IMPORT MODULES under test here:
-// import { example } from '../example.js';
+import { doesUserWin } from "../utilities.js";
 
 const test = QUnit.test;
 
-test('time to test a function', (expect) => {
-    //Arrange
-    // Set up your arguments and expectations
-    const expected = true;
-    
-    //Act 
-    // Call the function you're testing and set the result to a const
-    const actual = true;
+test('User: grass, CPU: grass', (expect) => {
+    const expected = 'tie';
+    const actual = doesUserWin('grass', 'grass');
+    expect.equal(actual, expected);
+});
 
-    //Expect
-    // Make assertions about what is expected versus the actual result
+test('User: fire, CPU: fire', (expect) => {
+    const expected = 'tie';
+    const actual = doesUserWin('fire', 'fire');
+    expect.equal(actual, expected);
+});
+
+test('User: water, CPU: water', (expect) => {
+    const expected = 'tie';
+    const actual = doesUserWin('water', 'water');
+    expect.equal(actual, expected);
+});
+
+
+
+
+test('User: grass, CPU: water', (expect) => {
+    const expected = 'win';
+    const actual = doesUserWin('grass', 'water');
+    expect.equal(actual, expected);
+});
+
+test('User: fire, CPU: grass', (expect) => {
+    const expected = 'win';
+    const actual = doesUserWin('fire', 'grass');
+    expect.equal(actual, expected);
+});
+
+test('User: water, CPU: fire', (expect) => {
+    const expected = 'win';
+    const actual = doesUserWin('water', 'fire');
+    expect.equal(actual, expected);
+});
+
+
+
+
+test('User: grass, CPU: fire', (expect) => {
+    const expected = 'lose';
+    const actual = doesUserWin('grass', 'fire');
+    expect.equal(actual, expected);
+});
+
+test('User: fire, CPU: water', (expect) => {
+    const expected = 'lose';
+    const actual = doesUserWin('fire', 'water');
+    expect.equal(actual, expected);
+});
+
+test('User: water, CPU: grass', (expect) => {
+    const expected = 'lose';
+    const actual = doesUserWin('water', 'grass');
     expect.equal(actual, expected);
 });
